47cb513390fb3fcb18be5cbda40fbb84cf4cb0cc
Jesse Bikman <jessebikman@gmail.com>
2014-02-19 10:53:22 -0500

    Added getopt functionality for args.
    ncfilename, outfile, NcVariableName, MinVal, MaxVal

--- adcirc_netcdf_viz.py
+++ adcirc_netcdf_viz.py
@@ -6,99 +6,129 @@ import netCDF4
 import datetime
 import time
 import sys
+import getopt
 
-imagefilename='test.png'
-shapefilename='test.shp'
-prjfilename = 'test.prj'
-titl='ADCIRC';
-url='twm_example.nc'
-#url='http://opendap.renci.org:1935/thredds/dodsC/ASGS/andrea/08/nc6b/blueridge.renci.org/fivemem/nhcConsensus/maxele.63.nc'
-# url = sys.argv[1]
-vname='maxele'
-#vname='zeta'
-#vname='zeta_max'
-# vname=sys.argv[2]
-start=datetime.datetime(2008, 9, 13, 6, 0, 0)
+def main(argv):
+    
+    # Defaults parameter values.
+    ncfilename='temp'
+    outfile='outShape'
+    NcVariableName='zeta_max'
+    MinVal = 0
+    MaxVal = 11
 
-# print 'Getting data from url ... '
-print 'Getting data from twm_example.nc ... '
+    try:
+        opts, args = getopt.getopt(argv,"hn:o:v:a:b:",["ncfilename=","outfile=", "NcVariableName=", "MinVal=", "MaxVal="])
+    except getopt.GetoptError:
+        print 'adcirc_netcdf_viz.py -n <ncfilename> -o <outfile> -v <NcVariableName> -a <MinVal> -b <MaxVal>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'adcirc_netcdf_viz.py -n <ncfilename> -o <outfile> -v <NcVariableName> -a <MinVal> -b <MaxVal>'
+            sys.exit()
+        elif opt in ("-n", "--ncfilename"):
+            ncfilename = arg
+        elif opt in ("-o", "--outfile"):
+            outfile = arg
+        elif opt in ("-v", "--NcVariableName"):
+            NcVariableName = arg
+        elif opt in ("-a", "--MinVal"):
+            MinVal = arg
+        elif opt in ("-b", "--MaxVal"):
+            MaxVal = arg
 
-time0=time.time()
-nc=netCDF4.Dataset(url).variables
-nc.keys()
-print nc.keys()
-lon = nc['x'][:]
-lat = nc['y'][:]
-# Move to 0-indexing by subtracting 1
-nv = nc['element'][:,:]-1
+    MinVal = float(MinVal)
+    MaxVal = float(MaxVal)
+    imagefilename=outfile+'.png'
+    shapefilename=outfile+'.shp'
+    prjfilename=outfile+'.prj'
 
-print "Shape of lon is (%i)" % lon.shape
-print "Shape of lat is (%i)" % lat.shape
-print "Shape of nv is (%i, %i)" % nv.shape
+    titl='ADCIRC';
+    url=ncfilename+'.nc'
+    vname=NcVariableName
+    start=datetime.datetime(2008, 9, 13, 6, 0, 0)
 
-print "Max of lon is (%i)" % lon.max()
-print "Max of lat is (%i)" % lat.max()
-print "Max of nv is (%i)" % nv.max()
+    print 'Getting data from url=%s... ' % url
 
-# time_var = nc['time']
-# dtime = netCDF4.num2date(time_var[:],time_var.units)
-#istart = netCDF4.date2index(start,time_var,select='nearest')
-#istart=1
-#var = nc[vname][istart,:]    
-var = nc[vname][:]
-print 'elapsed time= %d seconds' % (time.time()-time0)
-print "var[0]: " + str(var[0])
+    time0=time.time()
+    nc=netCDF4.Dataset(url).variables
+    nc.keys()
+    print nc.keys()
+    lon = nc['x'][:]
+    lat = nc['y'][:]
+    # Move to 0-indexing by subtracting 1
+    nv = nc['element'][:,:]-1
 
-print 'Triangulating ...'
-time0=time.time()
-tri = Tri.Triangulation(lon,lat, triangles=nv)
-print 'elapsed time= %d seconds' % (time.time()-time0)
+    print "Shape of lon is (%i)" % lon.shape
+    print "Shape of lat is (%i)" % lat.shape
+    print "Shape of nv is (%i, %i)" % nv.shape
 
-print 'Making contours in figure ...'
-time0=time.time()
-figure(figsize=(10,10))
-subplot(111,aspect=(1.0/cos(mean(lat)*pi/180.0)))
-levels = linspace(0.,5.,11)
-print 'Calling tricontourf  ...'
-contour = tricontourf(tri, var,levels=levels,shading='faceted')
-colorbar(orientation='horizontal')
-title(url)
-print 'elapsed time= %d seconds' % (time.time()-time0)
+    print "Max of lon is (%i)" % lon.max()
+    print "Max of lat is (%i)" % lat.max()
+    print "Max of nv is (%i)" % nv.max()
 
-# Uncomment to show matplotlib visualization.
-# show()
+    # time_var = nc['time']
+    # dtime = netCDF4.num2date(time_var[:],time_var.units)
+    #istart = netCDF4.date2index(start,time_var,select='nearest')
+    #istart=1
+    #var = nc[vname][istart,:]    
+    var = nc[vname][:]
+    print 'elapsed time= %d seconds' % (time.time()-time0)
+    print "var[0]: " + str(var[0])
 
-print "Saving figure as " +  imagefilename
-savefig(imagefilename)
+    print 'Triangulating ...'
+    time0=time.time()
+    tri = Tri.Triangulation(lon,lat, triangles=nv)
+    print 'elapsed time= %d seconds' % (time.time()-time0)
 
-time0=time.time()
-print 'Extracting contour shapes from tricontourf object ...'
-geoms = []
-# create list of tuples (geom, vmin, vmax)
-for colli,coll in enumerate(contour.collections):
-    vmin,vmax = contour.levels[colli:colli+2]
-    for p in coll.get_paths():
-        p.simplify_threshold = 0.0
-        polys = p.to_polygons()
-        # Don't append an empty polygon to the geometry or it will screw up the indexing.
-        if polys:
-            geoms.append( (Polygon(polys[0],polys[1:] ),vmin,vmax))
+    print 'Making contours in figure ...'
+    time0=time.time()
+    figure(figsize=(10,10))
+    subplot(111,aspect=(1.0/cos(mean(lat)*pi/180.0)))
+    levels = linspace(MinVal, MaxVal)
+    print 'Calling tricontourf  ...'
+    contour = tricontourf(tri, var,levels=levels,shading='faceted')
+    colorbar(orientation='horizontal')
+    title(url)
+    print 'elapsed time= %d seconds' % (time.time()-time0)
 
-print "Writing shapes to " + shapefilename
-schema = { 'geometry': 'Polygon', 'properties': { 'vmin': 'float', 'vmax': 'float' } }
-with fiona.open(shapefilename, 'w', 'ESRI Shapefile', schema) as c:
-    for geom in geoms:
-        c.write({
-            'geometry': mapping(geom[0]),
-            'properties': {'vmin': geom[1], 'vmax': geom[2]},
-        })
+    # Uncomment to show matplotlib visualization.
+    # show()
 
-print 'elapsed time= %d seconds' % (time.time()-time0)
-print "Writing prj to " + prjfilename
-prj_string = '''GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]'''
-prj_file = open(prjfilename, 'w')
-prj_file.write(prj_string)
-prj_file.close()
+    print "Saving figure as " +  imagefilename
+    savefig(imagefilename)
 
-# try reading the Shapefile back in 
-c = fiona.open(shapefilename, 'r')
+    time0=time.time()
+    print 'Extracting contour shapes from tricontourf object ...'
+    geoms = []
+    # create list of tuples (geom, vmin, vmax)
+    for colli,coll in enumerate(contour.collections):
+        vmin,vmax = contour.levels[colli:colli+2]
+        for p in coll.get_paths():
+            p.simplify_threshold = 0.0
+            polys = p.to_polygons()
+            # Don't append an empty polygon to the geometry or it will screw up the indexing.
+            if polys:
+                geoms.append( (Polygon(polys[0],polys[1:] ),vmin,vmax))
+
+    print "Writing shapes to " + shapefilename
+    schema = { 'geometry': 'Polygon', 'properties': { 'vmin': 'float', 'vmax': 'float' } }
+    with fiona.open(shapefilename, 'w', 'ESRI Shapefile', schema) as c:
+        for geom in geoms:
+            c.write({
+                'geometry': mapping(geom[0]),
+                'properties': {'vmin': geom[1], 'vmax': geom[2]},
+            })
+
+    print 'elapsed time= %d seconds' % (time.time()-time0)
+    print "Writing prj to " + prjfilename
+    prj_string = '''GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]'''
+    prj_file = open(prjfilename, 'w')
+    prj_file.write(prj_string)
+    prj_file.close()
+
+    # try reading the Shapefile back in 
+    c = fiona.open(shapefilename, 'r')
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
