ab39e0c88632783cb39592deb1cc06e8f4a06435
Jesse Bikman <jessebikman@gmail.com>
2014-03-11 13:14:28 -0400

    Updated tests

--- adcirc_local_test.py
+++ adcirc_local_test.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python
 """
-$ python netcdf_viz.py -n twm_example -o test -v maxele_prediction -a 0 -b 11
+$ python netcdf_viz.py -n twm_example -o test_local -v maxele_prediction -a 0 -b 11
 Getting data from url=twm_example.nc...
 [u'x', u'y', u'maxele', u'maxele_prediction', u'element', u'bnd']
 Shape of lon is (608114)
@@ -13,10 +13,10 @@ var[0]: -99999.0
 Triangulating ...
 Making contours in figure ...
 Calling tricontourf  ...
-Saving figure as test.png
+Saving figure as test_local.png
 Extracting contour shapes from tricontourf object ...
-Writing shapes to test.shp
-Writing prj to test.prj
+Writing shapes to test_local.shp
+Writing prj to test_local.prj
 
 """
 if __name__ == '__main__':
